36ce9969633a68d78f650bd543b1de53bf9bc7eb
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-05-20 16:21:40 +0300

    f

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Navigation from './components/Navigation/Navigation';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import UserInfo from './components/UserInfo/UserInfo';
-import ShoppingList from './components/ShoppingList/ShoppingList';
-import TodoList from './components/ToDoList/ToDoList';
+//todo imports
+import InputTodo from "./components/ToDoList/InputTodo";
+import ListTodos from "./components/ToDoList/ListTodos";
+// import TodoList from './components/ToDoList/ToDoList';
 import './App.css';
 
 const initialState = {
@@ -56,14 +58,8 @@ class App extends Component {
                 id={this.state.user.id}
               />
               <div className="cf-ns nl2 nr2">
-                <TodoList
-                  name={this.state.user.name}
-                  className="fl-ns w-25-ns ph2"
-                />
-                <ShoppingList
-                  name={this.state.user.name}
-                  className="fl-ns w-25-ns ph2"
-                />
+                <InputTodo />
+                <ListTodos />
               </div>
 
             </div>
