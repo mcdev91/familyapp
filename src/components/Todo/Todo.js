352e87fbecf8d3ae71b4dcddcda9a133e86b4496
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-05-31 20:31:48 +0300

    d

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,24 +1,14 @@
 import React from "react";
 
-const getTodos = async () => {
-    try {
-        const response = await fetch("https://listsoftodo.herokuapp.com/todos");
-        const jsonData = await response.json();
-        console.log(jsonData);
-    } catch (err) {
-        console.error(err.message);
-    }
-};
-
-// function getTodos() {
-//     fetch('https://listsoftodo.herokuapp.com/todos')
-//         .then(response => {
-//             return JSON.stringify(response);
-//         })
-//         .then(data => {
-//             console.log(data);
-//         });
-// }
+function getTodos() {
+    fetch('https://listsoftodo.herokuapp.com/todos')
+        .then(response => {
+            return JSON.stringify(response);
+        })
+        .then(data => {
+            console.log(data);
+        });
+}
 
 const Todo = () => {
     return (
