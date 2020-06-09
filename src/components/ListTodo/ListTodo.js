eaa02226fc7f25e570e03f183d3dee924e7c79f0
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-06-09 21:26:08 +0300

    t

--- src/components/ListTodo/ListTodo.js
+++ src/components/ListTodo/ListTodo.js
@@ -1,52 +1,29 @@
 import React, { Component } from 'react';
 
-// class ListTodo extends Component {
-//     constructor(props) {
-//         super(props);
+class ListTodo extends Component {
+    constructor(props) {
+        super(props);
 
-//         this.state = {
-//             data: null,
-//         };
-//     }
+        this.state = {
+            data: null,
+        };
+    }
 
-//     componentDidMount() {
-//         fetch('https://safe-basin-15384.herokuapp.com/todos')
-//             .then(response => response.json())
-//             .then(data => this.setState({ data }));
-//     }
-//     render() {
-//         return (
-//             <ul>
-//                 <div>
+    componentDidMount() {
+        fetch('https://safe-basin-15384.herokuapp.com/todos')
+            .then(response => response.json())
+            .then(data => this.setState({ data }));
+    }
+    render() {
+        return (
+            <ul>
+                <div>
 
-//                 </div>
-//             </ul>
-//         );
-//     }
-// }
+                </div>
+            </ul>
+        );
+    }
+}
 
-componentDidMount() 
-    var that = this;
-    var url = 'https://safe-basin-15384.herokuapp.com/todos'
-
-    fetch(url)
-        .then(function (response) {
-            if (response.status >= 400) {
-                throw new Error("Bad response from server");
-            }
-            return response.json();
-        })
-        .then(function (data) {
-            that.setState({ todo: data.description });
-        });
-
-
-const ListTodo = Object.keys(this.state.description.loc).map((content, idx) => {
-    const items = this.state.description.loc[content].map((item, i) => (
-        <p key={i}>{item.text}</p>
-    ))
-
-    return <div key={idx}>{items}</div>
-})
 
 export default ListTodo;
