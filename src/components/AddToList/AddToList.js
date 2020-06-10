05021d870d4454a8905af6dd8b106c15f231636d
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-06-10 21:18:59 +0300

    g

---
+++ src/components/AddToList/AddToList.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+class AddToList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: ''
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({ description: event.target.value })
+    }
+
+    onSubmitSignIn = () => {
+        fetch('https://safe-basin-15384.herokuapp.com/register', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                description: this.state.description
+            })
+        })
+            .then(response => response.json())
+            // .then(user => {
+            //     if (user.id) {
+            //         this.props.loadUser(user);
+            //         this.props.onRouteChange('home');
+            //     }
+            // })
+    }
+
+    render() {
+        return (
+            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <main className="pa4 white-80">
+                    <div className="measure">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0" />
+                        <legend className="f1 fw6 ph0 mh0 center">ADD TO LIST</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="text">TASK</label>
+                            <input
+                                className="pa2 input-reset ba bg-transparent hover-white w-100 outline-0"
+                                type="text"
+                                name="todo"
+                                id="todotask"
+                                onChange={this.onInputChange}
+                            />
+                        </div>
+                        <div>
+                            <input
+                                onClick={this.onSubmitSignIn}
+                                className="white ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib outline-0"
+                                type="submit"
+                                value="addToList"
+                            />
+                        </div>
+                    </div>
+                </main>
+            </article>
+        )
+    }
+}
+
+export default Register;
