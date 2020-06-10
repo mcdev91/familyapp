f82080d615ac81fdd72e606bb45733c0a8fac1b8
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-06-10 20:46:12 +0300

    g

--- src/components/ListTodo/ListTodo.js
+++ src/components/ListTodo/ListTodo.js
@@ -1,65 +1,51 @@
 import React, { Component } from 'react';
 
-class ListTodo extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            projectInfo: null,
+class ListTodo extends React.Component {
+    state = {
+      isLoading: true,
+      description: [],
+      error: null
+    };
+  
+    fechtodos() {
+      fetch(`https://safe-basin-15384.herokuapp.com/todos`)
+        .then(response => response.json())
+        .then(data =>
+          this.setState({
+            description: data,
             isLoading: false,
-            error: null
-        };
+          })
+        )
+        .catch(error => this.setState({ error, isLoading: false }));
     }
-
+  
     componentDidMount() {
-        this.setState({ isLoading: true });
-
-        let projectInfoApi;
-
-        projectInfoApi = 'https://safe-basin-15384.herokuapp.com/todos' + this.props.projectId;
-
-        fetch(projectInfoApi)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Encountered problem fetching project info')
-                }
-            })
-            .then(data => this.setState({
-                projectInfo: data
-            }))
-            .catch(fetchError => this.setState({
-                isLoading: false,
-                error: fetchError
-            }));
+      this.fechtodos();
     }
-
-    componentWillUnmount() {
-        // this.setState({
-        //     projectInfo: null
-        // });
-    }
-
     render() {
-        const { isLoading, error } = this.state;
-
-        if (error) {
-            return <p>{error.message}</p>
-        }
-
-        if (isLoading) {
-            return <p>Loading...</p>
-        }
-
-        return (
-            <div className="myReportSummary">
-                 Summary of Project name: {this.projectInfo.name}                    
-                 Number of events: {this.props.data.length}
-            </div>
-        );
+      const { isLoading, description, error } = this.state;
+      return (
+        <div>
+          <h1>Random Todo</h1>
+          {error ? <p>{error.message}</p> : null}
+          {!isLoading ? (
+            description.map(todo => {
+              const { todo_id, description } = todo;
+              return (
+                <div key={}>
+                  <p>Name: {todo_id}</p>
+                  <p>Email Address: {description}</p>
+                  <hr />
+                </div>
+              );
+            })
+          ) : (
+            <h3>Loading...</h3>
+          )}
+          </div>
+      );
+    }
   }
-}
 
 // class ListTodo extends Component {
 //     constructor(props) {
@@ -74,9 +60,7 @@ class ListTodo extends Component {
 //         fetch('https://safe-basin-15384.herokuapp.com/todos')
 //             .then(response => response.json())
 //             .then(data => this.setState({ data }));
-//         console.log(this.state.data);
 //     }
-
 //     render() {
 //         return (
 //             <ul>
