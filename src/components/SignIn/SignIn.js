215eb9960cc42d943907bfa2a317106c4e9d47dc
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-06-14 21:35:44 +0300

    g

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -38,7 +38,7 @@ class SignIn extends React.Component {
     render() {
         const { onRouteChange } = this.props;
         return (
-            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center f6 f2-m f-subheadline-l fw6 tc">
+            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 white-80">
                     <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0" />
