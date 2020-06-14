f9c06c7dd3dba204d98180eda5e259259fae0fb3
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-06-14 21:01:05 +0300

    f

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,13 +7,13 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
                 <p className='f3 link dim white pa3 pointer' onClick={() => onRouteChange('signout')}>Sign Out</p>
             </nav>
         );
-        // } else {
-        //     return (
-        //         <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        //             <p className='f3 link dim white pa3 pointer' onClick={() => onRouteChange('signin')}>Sign In</p>
-        //             <p className='f3 link dim white pa3 pointer' onClick={() => onRouteChange('register')}>Register</p>
-        //         </nav>
-        //     );
+    } else {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <p className='f3 link dim white pa3 pointer' onClick={() => onRouteChange('signin')}>Sign In</p>
+                <p className='f3 link dim white pa3 pointer' onClick={() => onRouteChange('register')}>Register</p>
+            </nav>
+        );
     }
 }
 
